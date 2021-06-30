1124a1443f02fdd2362bb1580c827e78047b7749
BenFrancka <benfrancka@gmail.com>
2021-06-30 14:03:04 -0700

    adds react router framework to app.js

--- src/App.js
+++ src/App.js
@@ -1,25 +1,77 @@
-import logo from './logo.svg';
+import React, { Component } from 'react';
+
+import {
+  BrowserRouter as Router,
+  Switch,
+  Route,
+  Link,
+  useParams,
+  Redirect
+} from 'react-router-dom';
+
+import Home from './Home';
+import Login from './Login';
+import SignUp from './SignUp';
+import Todos from './Todos';
 import './App.css';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
-}
+const TOKEN = 'TOKEN';
 
-export default App;
+export default class App extends Component {
+
+  state = {
+    token: localStorage.getItem(TOKEN)
+  }
+
+  login = (clientToken) => {
+    this.setState({ token: clientToken })
+    localStorage.setItem(TOKEN, clientToken)
+  }
+
+  logout = () => {
+    this.setState({ token: '' })
+    localStorage.setItem('TOKEN', '')
+  }
+
+  render() {
+    return(
+      <Router>
+        <div>
+          <div>
+            <Link to="/">Home</Link>
+            <Link to="/login">Login</Link>
+            <Link to="/signup">Sign Up</Link>
+            <Link to="/todos">Todos</Link>
+            <button onClick={this.logout}>Logout</button>
+          </div>
+          <Switch>
+            <Route
+              path="/" 
+              exact
+              render={(routerProps) => <Home {...routerProps} />}
+            />
+            <Route
+              path="/signup" 
+              exact
+              render={(routerProps) => <SignUp {...routerProps} />}
+            />
+            <Route
+              path="/login" 
+              exact
+              render={(routerProps) => <Login {...routerProps} />}
+            />
+            <Route 
+              path="/todos" 
+              exact
+              render={(routerProps) => 
+                this.state.token 
+                  ? <Todos {...routerProps} token={this.state.token} />
+                  : <Redirect to='/' />
+              }
+              />
+           </Switch>
+        </div>
+      </Router>
+    );
+  }
+}
