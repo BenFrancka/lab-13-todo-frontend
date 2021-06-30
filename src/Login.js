01395fd35fcf250cab22bda9d85e8c4d8522d88a
BenFrancka <benfrancka@gmail.com>
2021-06-30 14:49:17 -0700

    adds login page state, html elements, and event handlers

--- src/Login.js
+++ src/Login.js
@@ -1,10 +1,45 @@
 import React, { Component } from 'react'
+import { login } from './utils';
 
 export default class Login extends Component {
+
+    state = {
+        email: '',
+        password: ''
+    }
+
+    handleSubmit = async e => {
+        e.preventDefault();
+
+        const token = await login(this.state.email, this.state.password);
+
+        this.props.login(token)
+        this.props.history.push('/todos');
+    }
+
+    handleEmail = e => {
+        this.setState({ email: e.target.value });
+    }
+
+    handlePassword = e => {
+        this.setState({ password: e.target.value });
+    }
+
+
     render() {
         return (
             <div>
-                Login
+                <form onSubmit={this.handleSubmit}>
+                    <label>
+                        Email
+                        <input type="email" onChange={this.handleEmail}/>
+                    </label>
+                    <label>
+                        Password
+                        <input type="password" onChange={this.handlePassword}/>
+                    </label>
+                    <button>Create Account</button>
+                </form>
             </div>
         )
     }
