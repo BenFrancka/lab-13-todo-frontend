9067b890dc177c8a5255ba8533a64616cf7ca90a
BenFrancka <benfrancka@gmail.com>
2021-06-30 14:33:08 -0700

    adds signup-page state, html elements, and event handlers

--- src/SignUp.js
+++ src/SignUp.js
@@ -1,10 +1,45 @@
 import React, { Component } from 'react'
+import { signup } from './utils';
 
 export default class SignUp extends Component {
+
+    state = {
+        email: '',
+        password: ''
+    }
+
+    handleSubmit = async e => {
+        e.preventDefault();
+
+        const token = await signup(this.state.email, this.state.password);
+
+        this.props.login(token)
+        this.props.history.push('/todos');
+    }
+
+    handleEmail = e => {
+        this.setState({ email: e.target.value });
+    }
+
+    handlePassword = e => {
+        this.setState({ password: e.target.value });
+    }
+
+
     render() {
         return (
             <div>
-                Sign Up
+                <form onSubmit={this.handleSubmit}>
+                    <label>
+                        Email
+                        <input type="email" onChange={this.handleEmail}/>
+                    </label>
+                    <label>
+                        Password
+                        <input type="password" onChange={this.handlePassword}/>
+                    </label>
+                    <button>Create Account</button>
+                </form>
             </div>
         )
     }
